cfc69148246a4a71920a00c75a1fcaa8d817fd29
Suhas-GH <hosallisuhas3@gmail.com>
2025-03-26 11:15:29 +0530

    Fixed lombok issue

--- src/main/java/com/prototype/JournalApplication/entity/JournalEntry.java
+++ src/main/java/com/prototype/JournalApplication/entity/JournalEntry.java
@@ -1,16 +1,13 @@
 package com.prototype.JournalApplication.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+]
 import lombok.NonNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-@Data
 @Document(collection = "journal _entries")
-@NoArgsConstructor
 public class JournalEntry {
     @Id
     private long id;
@@ -23,4 +20,39 @@ public class JournalEntry {
 
     private LocalDateTime date;
 
+    public JournalEntry() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
 }
--- src/main/java/com/prototype/JournalApplication/entity/User.java
+++ src/main/java/com/prototype/JournalApplication/entity/User.java
@@ -1,6 +1,6 @@
 package com.prototype.JournalApplication.entity;
 
-import lombok.Data;
+
 import lombok.NonNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection= "Users")
-@Data
 public class User {
     @Id
     private String userId;
@@ -25,4 +24,46 @@ public class User {
 
     @DBRef
     private List<JournalEntry> journalEntries = new ArrayList<>();
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<JournalEntry> getJournalEntries() {
+        return journalEntries;
+    }
+
+    public void setJournalEntries(List<JournalEntry> journalEntries) {
+        this.journalEntries = journalEntries;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", journalEntries=" + journalEntries +
+                '}';
+    }
 }
